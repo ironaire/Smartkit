b7d6d884893837a40ccafbbb873fa55a4a04ff2f
Shunchao Wang <shunchao.wang@gmail.com>
2014-02-13 13:59:11 -0500

    Added Emacs content.

---
+++ Emacs/org-clock-save.el
@@ -0,0 +1,2 @@
+;; org-persist.el - swang-workstation at <2014-02-13 Thu 13:47>
+(setq stored-clock-history '(("/Users/swang/.emacs.d/org/refile.org" . 3192) ("/Users/swang/.emacs.d/org/ACR.org" . 119)))

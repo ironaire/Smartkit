aff4d0ea9142ad662402f73afa8a57e6790b5312
Shunchao Wang <shunchao.wang@gmail.com>
2014-02-18 08:45:11 -0500

    Added CIEHLO org file.

--- Emacs/org-clock-save.el
+++ Emacs/org-clock-save.el
@@ -1,2 +1,1 @@
-;; org-persist.el - swang-workstation at <2014-02-15 Sat 17:48>
-(setq stored-clock-history '(("/Users/swang/workbench-repo/Emacs/org/refile.org" . 3192) ("/Users/swang/.emacs.d/org/CIEHLO.org" . 121) ("/Users/swang/.emacs.d/org/CIEHLO.org" . 665) ("/Users/swang/.emacs.d/org/CIEHLO.org" . 122)))
+;; org-persist.el - swang-workstation at <2014-02-17 Mon 13:57>

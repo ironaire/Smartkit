62bea74b077b8b51630b0add9ae2730bee309333
Shunchao Wang <shunchao.wang@gmail.com>
2014-03-11 12:33:49 -0400

    Add sourcing of bash project script.

--- Emacs/org-clock-save.el
+++ Emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - swang-workstation at <2014-03-06 Thu 09:18>
+;; org-persist.el - swang-workstation at <2014-03-11 Tue 12:01>
 (setq stored-clock-history '(("/Users/swang/Workbench/Emacs/org/CIEHLO.org" . 1469)))

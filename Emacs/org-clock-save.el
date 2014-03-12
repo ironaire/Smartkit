7ef639e3db03a7749faa46d9a39dc28e8e6ed02c
Shunchao Wang <shunchao.wang@gmail.com>
2014-03-12 15:42:06 -0400

    Updated bashrc.

--- Emacs/org-clock-save.el
+++ Emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - swang-workstation at <2014-03-11 Tue 12:01>
+;; org-persist.el - swang-workstation at <2014-03-12 Wed 15:39>
 (setq stored-clock-history '(("/Users/swang/Workbench/Emacs/org/CIEHLO.org" . 1469)))

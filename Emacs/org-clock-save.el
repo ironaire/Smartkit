eef21ba947a5fe5b84242974086aba24462d7fd9
Shunchao Wang <shunchao.wang@gmail.com>
2014-03-18 10:57:13 -0400

    Added vim notes plugin and setting.

--- Emacs/org-clock-save.el
+++ Emacs/org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-persist.el - swang-workstation at <2014-03-14 Fri 10:53>
+;; org-persist.el - swang-workstation at <2014-03-18 Tue 09:52>
+(setq stored-clock-history '(("/Users/swang/Workbench/Emacs/org/CIEHLO.org" . 1887) ("/Users/swang/Workbench/Emacs/org/CIEHLO.org" . 2075) ("/Users/swang/Workbench/Emacs/org/CIEHLO.org" . 1888)))

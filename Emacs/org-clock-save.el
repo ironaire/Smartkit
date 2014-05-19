4ea512ed1d303a2a0447b2f76164f4c79bbb5327
Shunchao Wang <shunchao.wang@gmail.com>
2014-05-19 14:35:04 -0400

    Ignreed close save.

--- Emacs/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - swang-macbookpro at <2014-05-17 Sat 09:32>
-(setq stored-clock-history '(("/Users/swang/Workbench/Emacs/org/Smartory.org" . 123) ("/Users/swang/Workbench/Emacs/org/refile.org" . 3074) ("/Users/swang/Workbench/Emacs/org/Smartory.org" . 411) ("/Users/swang/Workbench/Emacs/org/diary.org" . 46)))

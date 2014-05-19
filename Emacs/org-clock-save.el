f4cc28bcf65ba518598144b2eff19d88b31abcfa
Shunchao Wang <shunchao.wang@gmail.com>
2014-05-19 14:31:09 -0400

    Fixed conflicts.

--- Emacs/org-clock-save.el
+++ Emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - swang-workstation at <2014-05-16 Fri 08:24>
-(setq stored-clock-history '(("/Users/swang/Workbench/Emacs/org/refile.org" . 3074) ("/Users/swang/Workbench/Emacs/org/refile.org" . 3074) ("/Users/swang/.emacs.d/org/Smartory.org" . 123) ("/Users/swang/Workbench/Emacs/org/refile.org" . 3074) ("/Users/swang/.emacs.d/org/Smartory.org" . 404) ("/Users/swang/.emacs.d/org/diary.org" . 46)))
+;; org-persist.el - swang-macbookpro at <2014-05-17 Sat 09:32>
+(setq stored-clock-history '(("/Users/swang/Workbench/Emacs/org/Smartory.org" . 123) ("/Users/swang/Workbench/Emacs/org/refile.org" . 3074) ("/Users/swang/Workbench/Emacs/org/Smartory.org" . 411) ("/Users/swang/Workbench/Emacs/org/diary.org" . 46)))
